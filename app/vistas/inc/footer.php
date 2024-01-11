7b985566ba6afc8077284a68ea427a615bd617ad
Infiniti727 <ancamaju.jaf@gmail.com>
2024-01-11 17:29:49 +0100

    quitado cosas innecesarias del footer y movidas algunas al header

--- app/vistas/inc/footer.php
+++ app/vistas/inc/footer.php
@@ -1,4 +1,4 @@
-<a>Usuario: <?php echo $_SESSION["username"] ?></a> <a href="<?php echo RUTA_URL ;?> /paginas/pagina_principal/">Home</a> <a href="<?php echo RUTA_URL ;?> /paginas/carrito/">Ver carrito</a> <a href="<?php echo RUTA_URL ;?> /paginas/cerrar/">Cerrar sesion</a>
+
 <script type="text/javascript" src="<?php echo RUTA_URL; ?>/js/main.js"></script>
 </body>
 </html>
--- app/vistas/inc/header.php
+++ app/vistas/inc/header.php
@@ -8,4 +8,5 @@
     <title><?php echo NOMBRESITIO; ?> </title>
 </head>
 <body>
+<a>Usuario: <?php echo $_SESSION["username"] ?></a> <a href="<?php echo RUTA_URL ;?> /paginas/pagina_principal/">Home</a> <a href="<?php echo RUTA_URL ;?> /paginas/cerrar/">Cerrar sesion</a>
     

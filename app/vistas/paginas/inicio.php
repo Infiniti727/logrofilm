02abc040dc81759fc67889cb0c944552c26f636a
Infiniti727 <ancamaju.jaf@gmail.com>
2024-01-11 17:56:57 +0100

    cambio al inicio de sesion

--- app/vistas/paginas/inicio.php
+++ app/vistas/paginas/inicio.php
@@ -1,12 +1,21 @@
-<?php require_once RUTA_APP.'/vistas/inc/header.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="id=edge">
+    <link rel="stylesheet" type="text/css" href="<?php echo RUTA_URL?>/css/estilos.css">
+    <title><?php echo NOMBRESITIO; ?> </title>
+</head>
+<body>
 <form action="<?php echo RUTA_URL ;?> /paginas/validacion/" method="post">
-            <label for="usuario">usuario:</label>
+            <label for="usuario">Usuario:</label>
             <input type="text" id="usuario" name="usuario" required><br>
 
             <label for="contraseña">Contraseña:</label>
             <input type="password" id="contraseña" name="contraseña" required><br>
 
-            <input type="submit" value="Enviar">
+            <input type="submit" value="Iniciar sesion">
     </form>
 </body>
 </html>

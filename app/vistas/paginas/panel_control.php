94b0fa5ccfed58819c079eb1a0dbf14ba005ee56
Infiniti727 <ancamaju.jaf@gmail.com>
2024-01-16 21:23:16 +0100

    pequeños cambios a la interfaz

--- app/vistas/paginas/panel_control.php
+++ app/vistas/paginas/panel_control.php
@@ -1,6 +1,6 @@
 <?php require_once RUTA_APP.'/vistas/inc/header.php';?>
 <h1 class="titulo_panel">PANEL DE CONTROL</h1>
-<div>
+<div class="m-auto text-center">
     <a class="btn" href="<?php echo RUTA_URL ;?> /paginas/panelP/">Panel de peliculas</a>
     <a class="btn" href="<?php echo RUTA_URL ;?> /paginas/panelU/">Panel de usuarios</a>
 </div>

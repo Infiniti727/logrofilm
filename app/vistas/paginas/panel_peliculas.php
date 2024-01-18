6cbf789e4376a97f654d16e062fddc6d775d2c89
Infiniti727 <ancamaju.jaf@gmail.com>
2024-01-18 21:44:46 +0100

    panel peliculas de verdad

--- app/vistas/paginas/panel_peliculas.php
+++ app/vistas/paginas/panel_peliculas.php
@@ -1,7 +1,14 @@
 <?php require_once RUTA_APP.'/vistas/inc/header.php';?>
-<h1 class="titulo_panel">PANEL DE CONTROL</h1>
+<h1 class="titulo_panel">PANEL DE PELICULAS</h1>
 <div>
-    <button>Panel de peliculas</button>
-    <button>Panel de usuarios</button>
+    <table class="table table-striped">
+        <tr>
+            <td>Id</td>
+            <td>Id Filmaffinity</td>
+            <td>Nombre</td>
+            <td>Nombre español</td>
+            <td>descripcion</td>
+        </tr>
+    </table>
 </div>
 <?php require_once RUTA_APP.'/vistas/inc/footer.php';?>
